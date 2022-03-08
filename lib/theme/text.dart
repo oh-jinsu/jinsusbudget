25e6e8a94e46cb2fd5739c2a1a407e61737f9ea1
오진수 <ohjinsu@codersproduct.com>
2022-03-08 22:14:36 +0900

    Update the default font color

--- lib/theme/text.dart
+++ lib/theme/text.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 
-const textTheme = TextTheme(
+final textTheme = const TextTheme(
+  bodyMedium: TextStyle(),
   titleSmall: TextStyle(
-    color: Color(0xff111111),
+    color: Color(0xff323232),
     fontWeight: FontWeight.bold,
   ),
   titleMedium: TextStyle(
-    color: Color(0xff111111),
+    color: Color(0xff323232),
     fontWeight: FontWeight.bold,
   ),
   titleLarge: TextStyle(
-    color: Color(0xff111111),
+    color: Color(0xff323232),
     fontWeight: FontWeight.bold,
   ),
   headlineSmall: TextStyle(
-    color: Color(0xff111111),
+    color: Color(0xff323232),
     fontWeight: FontWeight.bold,
   ),
   headlineMedium: TextStyle(
-    color: Color(0xff111111),
+    color: Color(0xff323232),
     fontWeight: FontWeight.bold,
   ),
+).apply(
+  bodyColor: const Color(0xff323232),
+  displayColor: const Color(0xff323232),
 );
